1d558517cba76a19433e653926f32281e058e7a2
Jaime Pokhoy <jaimepokhoy@gmail.com>
2017-12-16 20:19:48 -0500

    Backend & Socket
    setup backend and socket io

--- frontend/lone/src/app/app.module.ts
+++ frontend/lone/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: '', component: HomeComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
